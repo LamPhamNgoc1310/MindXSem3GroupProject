12ab5a19fbe6c37febafadc9bba786926d3de639
LamPhamNgoc1310 <phamngoclam1310@gmail.com>
2024-10-04 14:55:54 +0700

    docs: uncomment Routers

--- Akademi/src/App.jsx
+++ Akademi/src/App.jsx
@@ -3,6 +3,7 @@ import { publicRouter, privateRouter } from "./router/router.jsx"
 import React from "react";
 import "./App.css";
 import LayoutTeacher from "./teacher/LayoutTeacher.jsx";
+import Layout from "./layout/Layout.jsx"
 
 function App() {
   const user = JSON.parse(localStorage.getItem('user'));
@@ -10,15 +11,24 @@ function App() {
   return (
     <>
       <Routes>
-        {/* {publicRouter.map((route, index) => {
+        {publicRouter.map((route, index) => {
           return (
             <Route path={route.path} element={route.component} key={index} />
           );
         })}
+
         {/* mỗi 1 role là 1 layout khác nhau nên để như này nhé ae! */}
         {user.role === 'Teacher' && (
           <Route path="/u" element={<LayoutTeacher />}>
             {privateRouter.teacher.map((route, index) => (
+              <Route path={route.path} element={route.component} key={index} />
+            ))}
+          </Route>
+        )}
+
+        {user.role === 'Admin' && (
+          <Route path="/admin" element={<Layout/>}>
+            {privateRouter.admin.map((route, index) => (
               <Route path={route.path} element={route.component} key={index} />
             ))}
           </Route>
--- Akademi/src/router/router.jsx
+++ Akademi/src/router/router.jsx
@@ -1,64 +1,75 @@
 // teacher
-import Login from "../login/Login.jsx"
-import AddNewStudent from "../teacher/AddNewStudent.jsx"
-import Dashboard from "../teacher/Dashboard.jsx"
-import Event from "../teacher/Event.jsx"
-import StudentGrades from "../teacher/StudentGrades.jsx"
-import StudentsManagement from "../teacher/StudentsManagement.jsx"
-import TeacherTimeline from "../teacher/TeacherTimeline.jsx"
-import UserTeacher from "../teacher/UserTeacher.jsx"
+import Login from "../login/Login.jsx";
+import AddNewStudent from "../teacher/AddNewStudent.jsx";
+import Dashboard from "../teacher/Dashboard.jsx";
+import Event from "../teacher/Event.jsx";
+import StudentGrades from "../teacher/StudentGrades.jsx";
+import StudentsManagement from "../teacher/StudentsManagement.jsx";
+import TeacherTimeline from "../teacher/TeacherTimeline.jsx";
+import UserTeacher from "../teacher/UserTeacher.jsx";
+
+import Admin from "../admin/Admin.jsx";
+import AdminTeacherMan from "../admin/AdminTeacherMan.jsx";
+import AdminClassMan from "../admin/AdminClassMan.jsx";
+import AdminSchedule from "../admin/AdminSchedule.jsx";
+import AdminCurriculum from "../admin/AdminCurriculum.jsx";
 
 // admin
 
 // student
 
 const publicRouter = [
-
-    {
-        // path: '/signin', component: <Signin />
-    },
-    {
-        path: '/', component: <Login />
-    },
-]
+  {
+    path: "/",
+    component: <Login />,
+  },
+];
 
 const privateRouter = {
-    // Lâm - admin
-    // {
-    //     path: '/admin', component: <Admin />
-    // },
-    // {
-    //     path: '/admin/teacherman', component: <AdminTeacherMan />
-    // },
-    // {
-    //     path: '/admin/classman', component: <AdminClassMan />
-    // },
-    // {
-    //     path: '/admin/schedule', component: <AdminSchedule />
-    // },
-    // {
-    //     path: '/admin/curriculum', component: <AdminCurriculum />
-    // },
+  // Lâm - admin
+  admin: [
+    {
+      path: "/admin",
+      component: <Admin />,
+    },
+    {
+      path: "/admin/teacherman",
+      component: <AdminTeacherMan />,
+    },
+    {
+      path: "/admin/classman",
+      component: <AdminClassMan />,
+    },
+    {
+      path: "/admin/schedule",
+      component: <AdminSchedule />,
+    },
+    {
+      path: "/admin/curriculum",
+      component: <AdminCurriculum />,
+    },
+  ],
 
-    // Đức - teacher
-    teacher: [
-        { path: '/u/dashboard', component: <Dashboard /> },
-        { path: '/u/event', component: <Event /> },
-        { path: '/u/students', component: <StudentsManagement /> },
-        { path: '/u/students/add', component: <AddNewStudent /> },
-        { path: '/u/latest-activity', component: <TeacherTimeline /> },
-        { path: '/u/grades', component: <StudentGrades /> },
-        { path: '/u/userTeacher', component: <UserTeacher /> }],
-    // Quang - student
-    // {
-    //     path: '/student', component: <StudentPersonalInfo />
-    // },
-    // {
-    //     path: '/student/academic', component: <StudentAcademic />
-    // },
-    // {
-    //     path: '/student/schedule', component: <StudentSchedule />
-    // }
-}
+  // Đức - teacher
+  teacher: [
+    { path: "/u/dashboard", component: <Dashboard /> },
+    { path: "/u/event", component: <Event /> },
+    { path: "/u/students", component: <StudentsManagement /> },
+    { path: "/u/students/add", component: <AddNewStudent /> },
+    { path: "/u/latest-activity", component: <TeacherTimeline /> },
+    { path: "/u/grades", component: <StudentGrades /> },
+    { path: "/u/userTeacher", component: <UserTeacher /> },
+  ],
+  // Quang - student
+  // {
+  //     path: '/student', component: <StudentPersonalInfo />
+  // },
+  // {
+  //     path: '/student/academic', component: <StudentAcademic />
+  // },
+  // {
+  //     path: '/student/schedule', component: <StudentSchedule />
+  // }
+};
 
-export { publicRouter, privateRouter }
+export { publicRouter, privateRouter };
